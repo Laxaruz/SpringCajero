eae7a683b4406a30bb10fa17838243585010ce8f
Laxaruz <laxaruzrose@gmail.com>
2025-05-19 08:20:03 -0500

    Completando el controlador admin

--- Cajero/src/main/java/com/Laxaruz/Cajero/controller/AdminController.java
+++ Cajero/src/main/java/com/Laxaruz/Cajero/controller/AdminController.java
@@ -1,15 +1,14 @@
 package com.Laxaruz.Cajero.controller;
 
 import com.Laxaruz.Cajero.Entity.Cliente;
+import com.Laxaruz.Cajero.Entity.Cuenta;
+import com.Laxaruz.Cajero.Entity.TipoCuenta;
 import com.Laxaruz.Cajero.services.ClienteService;
 import com.Laxaruz.Cajero.services.CuentaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -26,13 +25,38 @@ public class AdminController {
     }
 
     @GetMapping("/crear-cliente")
-    public String mostrarFomularioCliente(Model model1) {
-        model1.addAttribute("cliente", new Cliente());
+    public String mostrarFomularioCliente(Model model) {
+        model.addAttribute("cliente", new Cliente());
         return "admin/crear-cliente";
     }
     @PostMapping("/crear-cliente")
     public String crearCliente(@ModelAttribute Cliente cliente) {
         clienteService.crearCliente(cliente);
         return "redirect:/admin"; // Redirige a la página de inicio del admin
+    }
+    @GetMapping("/crear-cuenta")
+    public String mostrarFormularioCuenta(Model model) {
+        model.addAttribute("cuenta", new Cuenta());
+        return "admin/crear-cuenta";
+    }
+    @PostMapping("/crear-cuenta")
+    public String crearCuenta(@RequestParam String identificacion,
+                              @RequestParam String numero,
+                              @RequestParam TipoCuenta tipo,
+                              @RequestParam double saldo) {
+        Cliente cliente = clienteService.buscarPorIdentificacion(identificacion).orElseThrow();
+        cuentaService.crearCuenta(cliente,numero,tipo,saldo);
+
+        return "redirect:/admin"; // Redirige a la página de inicio del admin
+    }
+    @GetMapping("/desbloquear")
+    public String mostrarDesbloqueo() {
+        return "admin/desbloquear";
+    }
+    @PostMapping("/desbloquear")
+    public String desbloquearCuenta(@RequestParam String identificacion,
+                                    @RequestParam String nuevoPin){
+        clienteService.desbloquearCliente(identificacion,nuevoPin);
+        return "redirect:/admin";
     }
 }
