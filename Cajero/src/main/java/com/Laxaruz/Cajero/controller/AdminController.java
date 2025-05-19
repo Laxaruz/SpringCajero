e933b7a2ca6582b352c8a2ad357b3947ad609454
Laxaruz <laxaruzrose@gmail.com>
2025-05-19 07:53:27 -0500

    creando: vista desbloquear.html, crear-cliente.html, crear-cuenta.html y los métodos para crear cliente

--- Cajero/src/main/java/com/Laxaruz/Cajero/controller/AdminController.java
+++ Cajero/src/main/java/com/Laxaruz/Cajero/controller/AdminController.java
@@ -1,10 +1,14 @@
 package com.Laxaruz.Cajero.controller;
 
+import com.Laxaruz.Cajero.Entity.Cliente;
 import com.Laxaruz.Cajero.services.ClienteService;
 import com.Laxaruz.Cajero.services.CuentaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,7 +20,19 @@ public class AdminController {
 
     //Creación de rutas, lo que va despues de /admin
     @GetMapping
-    public String adminHome(){
+    public String adminHome() {
+
         return "admin/index"; // Nombre de la vista
+    }
+
+    @GetMapping("/crear-cliente")
+    public String mostrarFomularioCliente(Model model1) {
+        model1.addAttribute("cliente", new Cliente());
+        return "admin/crear-cliente";
+    }
+    @PostMapping("/crear-cliente")
+    public String crearCliente(@ModelAttribute Cliente cliente) {
+        clienteService.crearCliente(cliente);
+        return "redirect:/admin"; // Redirige a la página de inicio del admin
     }
 }
